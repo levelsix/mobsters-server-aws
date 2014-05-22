cba81b3ff8c166bccb3ace4ece1e7ce788984f69
bluestix <endgame2013@gmail.com>
2014-05-22 11:48:12 -0700

    removed blacksmith history

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/BlacksmithAttemptHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lvl6.mobsters.info.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.lvl6.mobsters.info.BlacksmithAttemptHistory;
-public interface BlacksmithAttemptHistoryRepository extends CrudRepository<BlacksmithAttemptHistory, String>{
-
-}
