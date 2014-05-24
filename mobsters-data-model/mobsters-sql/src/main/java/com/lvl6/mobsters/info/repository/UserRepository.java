5a3e3d6505cbbf78ed195f370272b30be2d4c661
Arturo Wu Zhou <aweezy90@gmail.com>
2014-05-23 19:01:17 -0700

    removed mysql version of User

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lvl6.mobsters.info.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.lvl6.mobsters.info.User;
-public interface UserRepository extends CrudRepository<User, String>{
-
-}
