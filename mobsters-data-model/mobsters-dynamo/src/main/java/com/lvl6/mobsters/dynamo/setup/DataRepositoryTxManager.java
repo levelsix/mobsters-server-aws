68d07dcb65ed8cd453bc2f67ea50f136440fb1a1
John Heinnickel <john@lvl6.com>
2014-06-03 20:39:21 -0700

    Interfaces and Exceptions for Lvl6 DynamoDB Transaction manager bean.
    Will encapsulate the existing library and ensure that Services can
    control transaction lifecycle, while BaseDynamoDb is able to access and
    use the current thread's open transaction, if any.

---
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/setup/DataRepositoryTxManager.java
@@ -0,0 +1,12 @@
+package com.lvl6.mobsters.dynamo.setup;
+
+import com.amazonaws.services.dynamodbv2.transactions.Transaction;
+
+public interface DataRepositoryTxManager {
+	/**
+	 * Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns null.
+	 * 
+	 * @return Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns null.
+	 */
+    Transaction getActiveTransaction();
+}
---
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/setup/DataServiceTxManager.java
@@ -0,0 +1,36 @@
+package com.lvl6.mobsters.dynamo.setup;
+
+
+public interface DataServiceTxManager {
+	/**
+	 * Begin a new thread-scoped transaction if none is presently active.  If the current thread already has an active
+	 * transaction, an IllegalStateException is thrown instead.
+	 * 
+	 * @return Returns if a new transaction was successfully opened and bound to the current thread.
+	 * @throws IllegalStateException if an open transaction is already bound to the current thread.
+	 * @throws TransactionFailureException if something unexpectedly prevents a transaction from being opened.
+	 */
+	public void beginTransaction();
+
+	/**
+	 * Ends an existing thread-scoped transaction if one is presently active.  If the current thread lacks an active
+	 * transaction, an IllegalStateException is thrown instead.
+	 * 
+	 * @return Returns if an active transaction bound to the current thread was successfully committed.
+	 * @throws IllegalStateException if no active transaction is bound to the current thread.
+	 * @throws TransactionFailureException if something unexpectedly prevents the thread's active transaction from
+	 *         being committed.
+	 */
+	public void commit();
+	
+	/**
+	 * Begin a new thread-scoped transaction if none is presently active.  If the current thread already has an active
+	 * transaction, an IllegalStateException is thrown instead.
+	 * 
+	 * @return Returns if an active transaction bound to the current thread was successfully rolled back.
+	 * @throws IllegalStateException if no active transaction is bound to the current thread.
+	 * @throws TransactionFailureException if something unexpectedly prevents the thread's active transaction from
+	 *         being rolled back.
+	 */
+	public void rollback();
+}
