9f3b21ed7d06c79ee6ec026cc8463590764ce4cd
Arturo Wu Zhou <aweezy90@gmail.com>
2014-06-05 00:44:55 -0700

    fixed up hash key and range key assignment; copy pasted overriding createTable method

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/AchievementForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/AchievementForUserRepository.java
@@ -1,11 +1,79 @@
 package com.lvl6.mobsters.dynamo.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.GlobalSecondaryIndex;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.LocalSecondaryIndex;
 import com.lvl6.mobsters.dynamo.AchievementForUser;
-@Component public class AchievementForUserRepository extends BaseDynamoRepository<AchievementForUser>{
-	public AchievementForUserRepository(){
+
+@Component
+public class AchievementForUserRepository extends
+		BaseDynamoRepository<AchievementForUser> {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(QuestForUserRepository.class);
+
+	public AchievementForUserRepository() {
 		super(AchievementForUser.class);
-		isActive = true;//for unit test
+		isActive = true;// for unit test
+	}
+
+	@Override
+	protected void createTable() {
+		try {
+			log.info("Creating Dynamo table {}", getTableName());
+			ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+			attributeDefinitions.add(new AttributeDefinition()
+					.withAttributeName("userId").withAttributeType("S"));
+			attributeDefinitions.add(new AttributeDefinition()
+					.withAttributeName("achievementId").withAttributeType("N"));
+
+			ArrayList<KeySchemaElement> ks = new ArrayList<KeySchemaElement>();
+			ks.add(new KeySchemaElement().withAttributeName("userId")
+					.withKeyType(KeyType.HASH));
+			ks.add(new KeySchemaElement().withAttributeName("achievementId")
+					.withKeyType(KeyType.RANGE));
+
+			CreateTableRequest request = new CreateTableRequest()
+					.withTableName(getTableName())
+					.withAttributeDefinitions(attributeDefinitions)
+					.withKeySchema(ks)
+					.withProvisionedThroughput(provisionedThroughput);
+			List<GlobalSecondaryIndex> globalIndexes = getGlobalIndexes();
+			if (globalIndexes != null && !globalIndexes.isEmpty()) {
+				request.withGlobalSecondaryIndexes(globalIndexes);
+			}
+			List<LocalSecondaryIndex> localIndexes = getLocalIndexes();
+			if (localIndexes != null && !localIndexes.isEmpty()) {
+				request.withLocalSecondaryIndexes(localIndexes);
+			}
+			log.info("Creating table: {}", request);
+			CreateTableResult result = client.createTable(request);
+			log.info("Create table result: {}", result);
+		} catch (Throwable e) {
+			log.error("Error creating Dynamo table {}", getTableName(), e);
+			throw e;
+		}
+	}
+
+	@Override
+	public List<LocalSecondaryIndex> getLocalIndexes() {
+		return null;
+	}
+
+	@Override
+	public List<GlobalSecondaryIndex> getGlobalIndexes() {
+		return null;
 	}
 
 }
--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/QuestForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/QuestForUserRepository.java
@@ -1,4 +1,5 @@
 package com.lvl6.mobsters.dynamo.repository;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -20,141 +21,142 @@ import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.LocalSecondaryIndex;
 import com.lvl6.mobsters.dynamo.QuestForUser;
-@Component public class QuestForUserRepository extends BaseDynamoRepository<QuestForUser>{
-	
-	
-	private static final Logger log = LoggerFactory.getLogger(QuestForUserRepository.class);
-	
-	
-	public QuestForUserRepository(){
+
+@Component
+public class QuestForUserRepository extends BaseDynamoRepository<QuestForUser> {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(QuestForUserRepository.class);
+
+	public QuestForUserRepository() {
 		super(QuestForUser.class);
 		isActive = true;
 	}
 
-	
-	
-	public List<QuestForUser> getQuestsForUser(String userId, boolean isComplete, Collection<String> questIds){
+	public List<QuestForUser> getQuestsForUser(String userId,
+			boolean isComplete, Collection<String> questIds) {
 		List<AttributeValue> questIdz = new ArrayList<>();
 		QuestForUser hashKey = new QuestForUser();
 		hashKey.setUserId(userId);
-		for(String quest : questIds) {
+		for (String quest : questIds) {
 			questIdz.add(new AttributeValue().withS(quest));
 		}
-		
 
 		DynamoDBQueryExpression<QuestForUser> query = new DynamoDBQueryExpression<QuestForUser>()
-				//.withIndexName("userIdGlobalIndex")
+				// .withIndexName("userIdGlobalIndex")
 				.withHashKeyValues(hashKey)
-				.withQueryFilterEntry("isComplete", new Condition()
-					.withComparisonOperator(ComparisonOperator.NE)
-					.withAttributeValueList(new AttributeValue().withN(getBoolean(isComplete))))
-				.withQueryFilterEntry("questId", new Condition()
-					.withComparisonOperator(ComparisonOperator.IN)
-					.withAttributeValueList(questIdz))
-				.withConsistentRead(true);
-				
-				
+				.withQueryFilterEntry(
+						"isComplete",
+						new Condition().withComparisonOperator(
+								ComparisonOperator.NE).withAttributeValueList(
+								new AttributeValue()
+										.withN(getBoolean(isComplete))))
+				.withQueryFilterEntry(
+						"questId",
+						new Condition().withComparisonOperator(
+								ComparisonOperator.IN).withAttributeValueList(
+								questIdz)).withConsistentRead(true);
+
 		log.info("Query: {}", query);
-		PaginatedQueryList<QuestForUser> questsForUser = mapper.query(QuestForUser.class, query);
+		PaginatedQueryList<QuestForUser> questsForUser = mapper.query(
+				QuestForUser.class, query);
 		questsForUser.loadAllResults();
 		return questsForUser;
 	}
-	
-	
 
-	
-	
 	@Override
 	protected void createTable() {
 		try {
-		log.info("Creating Dynamo table {}", getTableName());
-		ArrayList<AttributeDefinition> attributeDefinitions= new ArrayList<AttributeDefinition>();
-		attributeDefinitions.add(new AttributeDefinition().withAttributeName("userId").withAttributeType("S"));
-		attributeDefinitions.add(new AttributeDefinition().withAttributeName("questId").withAttributeType("S"));
-		//attributeDefinitions.add(new AttributeDefinition().withAttributeName("questId").withAttributeType("S"));
-		
-		ArrayList<KeySchemaElement> ks = new ArrayList<KeySchemaElement>();
-		ks.add(new KeySchemaElement().withAttributeName("userId").withKeyType(KeyType.HASH));
-		ks.add(new KeySchemaElement().withAttributeName("questId").withKeyType(KeyType.RANGE));
-		
-		  
-		CreateTableRequest request = new CreateTableRequest()
-		    .withTableName(getTableName())
-		    .withAttributeDefinitions(attributeDefinitions)
-		    .withKeySchema(ks)
-		    .withProvisionedThroughput(provisionedThroughput);
+			log.info("Creating Dynamo table {}", getTableName());
+			ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+			attributeDefinitions.add(new AttributeDefinition()
+					.withAttributeName("userId").withAttributeType("S"));
+			attributeDefinitions.add(new AttributeDefinition()
+					.withAttributeName("questId").withAttributeType("N"));
+			// attributeDefinitions.add(new
+			// AttributeDefinition().withAttributeName("questId").withAttributeType("S"));
+
+			ArrayList<KeySchemaElement> ks = new ArrayList<KeySchemaElement>();
+			ks.add(new KeySchemaElement().withAttributeName("userId")
+					.withKeyType(KeyType.HASH));
+			ks.add(new KeySchemaElement().withAttributeName("questId")
+					.withKeyType(KeyType.RANGE));
+
+			CreateTableRequest request = new CreateTableRequest()
+					.withTableName(getTableName())
+					.withAttributeDefinitions(attributeDefinitions)
+					.withKeySchema(ks)
+					.withProvisionedThroughput(provisionedThroughput);
 			List<GlobalSecondaryIndex> globalIndexes = getGlobalIndexes();
-			if(globalIndexes != null && !globalIndexes.isEmpty()) {
+			if (globalIndexes != null && !globalIndexes.isEmpty()) {
 				request.withGlobalSecondaryIndexes(globalIndexes);
 			}
 			List<LocalSecondaryIndex> localIndexes = getLocalIndexes();
-			if(localIndexes != null && !localIndexes.isEmpty()) {
+			if (localIndexes != null && !localIndexes.isEmpty()) {
 				request.withLocalSecondaryIndexes(localIndexes);
 			}
-		    log.info("Creating table: {}", request);
-		CreateTableResult result = client.createTable(request);
-		log.info("Create table result: {}", result);
-		}catch(Throwable e) {
+			log.info("Creating table: {}", request);
+			CreateTableResult result = client.createTable(request);
+			log.info("Create table result: {}", result);
+		} catch (Throwable e) {
 			log.error("Error creating Dynamo table {}", getTableName(), e);
 			throw e;
 		}
 	}
-	
+
 	@Override
-	public List<LocalSecondaryIndex> getLocalIndexes(){
-		/*List<LocalSecondaryIndex> indexes = new ArrayList<>();
-		//isCompleteIndex
-		ArrayList<KeySchemaElement> indexKeySchema = new ArrayList<KeySchemaElement>();
-		indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
-		indexKeySchema.add(new KeySchemaElement("questId", KeyType.RANGE));
-		indexes.add(new LocalSecondaryIndex()
-			.withIndexName("questIdIndex")
-			.withKeySchema(indexKeySchema)
-			.withProjection(new Projection().withProjectionType(ProjectionType.ALL)));
-		//isRedeemedIndex
-		indexKeySchema = new ArrayList<KeySchemaElement>();
-		indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
-		indexKeySchema.add(new KeySchemaElement("isRedeemed", KeyType.RANGE));
-		indexes.add(new LocalSecondaryIndex()
-			.withIndexName("isRedeemedIndex")
-			.withKeySchema(indexKeySchema)
-			.withProjection(new Projection().withProjectionType(ProjectionType.ALL)));
-		//isCompleteIndex
-				indexKeySchema = new ArrayList<KeySchemaElement>();
-				indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
-				indexKeySchema.add(new KeySchemaElement("isComplete", KeyType.RANGE));
-				indexes.add(new LocalSecondaryIndex()
-					.withIndexName("isRedeemedIndex")
-					.withKeySchema(indexKeySchema)
-					.withProjection(new Projection().withProjectionType(ProjectionType.ALL)));*/
+	public List<LocalSecondaryIndex> getLocalIndexes() {
+		/*
+		 * List<LocalSecondaryIndex> indexes = new ArrayList<>();
+		 * //isCompleteIndex ArrayList<KeySchemaElement> indexKeySchema = new
+		 * ArrayList<KeySchemaElement>(); indexKeySchema.add(new
+		 * KeySchemaElement("userId", KeyType.HASH)); indexKeySchema.add(new
+		 * KeySchemaElement("questId", KeyType.RANGE)); indexes.add(new
+		 * LocalSecondaryIndex() .withIndexName("questIdIndex")
+		 * .withKeySchema(indexKeySchema) .withProjection(new
+		 * Projection().withProjectionType(ProjectionType.ALL)));
+		 * //isRedeemedIndex indexKeySchema = new ArrayList<KeySchemaElement>();
+		 * indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
+		 * indexKeySchema.add(new KeySchemaElement("isRedeemed",
+		 * KeyType.RANGE)); indexes.add(new LocalSecondaryIndex()
+		 * .withIndexName("isRedeemedIndex") .withKeySchema(indexKeySchema)
+		 * .withProjection(new
+		 * Projection().withProjectionType(ProjectionType.ALL)));
+		 * //isCompleteIndex indexKeySchema = new ArrayList<KeySchemaElement>();
+		 * indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
+		 * indexKeySchema.add(new KeySchemaElement("isComplete",
+		 * KeyType.RANGE)); indexes.add(new LocalSecondaryIndex()
+		 * .withIndexName("isRedeemedIndex") .withKeySchema(indexKeySchema)
+		 * .withProjection(new
+		 * Projection().withProjectionType(ProjectionType.ALL)));
+		 */
 		return null;
 	}
-	
-	
-	
+
 	@Override
-	public List<GlobalSecondaryIndex> getGlobalIndexes(){
-/*		List<GlobalSecondaryIndex> indexes = new ArrayList<>();
-		//userId
-		ArrayList<KeySchemaElement> indexKeySchema = new ArrayList<KeySchemaElement>();
-		indexKeySchema.add(new KeySchemaElement("userId", KeyType.HASH));
-		indexes.add(new GlobalSecondaryIndex()
-			.withIndexName("userIdGlobalIndex")
-			.withKeySchema(indexKeySchema)
-			.withProjection(new Projection().withProjectionType(ProjectionType.ALL))
-			.withProvisionedThroughput(provisionedThroughput));*/
-		
-		//questId
-/*		indexKeySchema = new ArrayList<KeySchemaElement>();
-		indexKeySchema.add(new KeySchemaElement("questId", KeyType.HASH));
-		indexes.add(new GlobalSecondaryIndex()
-			.withIndexName("questIdGlobalIndex")
-			.withKeySchema(indexKeySchema)
-			.withProjection(new Projection().withProjectionType(ProjectionType.ALL))
-			.withProvisionedThroughput(provisionedThroughput));
-		*/
+	public List<GlobalSecondaryIndex> getGlobalIndexes() {
+		/*
+		 * List<GlobalSecondaryIndex> indexes = new ArrayList<>(); //userId
+		 * ArrayList<KeySchemaElement> indexKeySchema = new
+		 * ArrayList<KeySchemaElement>(); indexKeySchema.add(new
+		 * KeySchemaElement("userId", KeyType.HASH)); indexes.add(new
+		 * GlobalSecondaryIndex() .withIndexName("userIdGlobalIndex")
+		 * .withKeySchema(indexKeySchema) .withProjection(new
+		 * Projection().withProjectionType(ProjectionType.ALL))
+		 * .withProvisionedThroughput(provisionedThroughput));
+		 */
+
+		// questId
+		/*
+		 * indexKeySchema = new ArrayList<KeySchemaElement>();
+		 * indexKeySchema.add(new KeySchemaElement("questId", KeyType.HASH));
+		 * indexes.add(new GlobalSecondaryIndex()
+		 * .withIndexName("questIdGlobalIndex") .withKeySchema(indexKeySchema)
+		 * .withProjection(new
+		 * Projection().withProjectionType(ProjectionType.ALL))
+		 * .withProvisionedThroughput(provisionedThroughput));
+		 */
 		return null;
 	}
-	
-	
+
 }
