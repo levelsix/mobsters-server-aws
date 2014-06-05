6b5850e94a25344937a54f0a131b2324a99f701b
John Heinnickel <john@lvl6.com>
2014-06-05 16:46:08 -0700

    Lvl6 Transaction Context Management, v1.0
    Adding DynamoDB Transaction-library based transaction context manager
    for reuse by data layer services (to start/end/rollback transactions)
    and arranged for its Spring wiring.  This is a first revision--some
    tuning for exception handling and debugging should be expected.

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/setup/DataRepositoryTxManager.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/setup/DataRepositoryTxManager.java
@@ -1,12 +1,36 @@
 package com.lvl6.mobsters.dynamo.setup;
 
 import com.amazonaws.services.dynamodbv2.transactions.Transaction;
+import com.amazonaws.services.dynamodbv2.transactions.Transaction.IsolationLevel;
 
-public interface DataRepositoryTxManager {
-	/**
-	 * Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns null.
-	 * 
-	 * @return Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns null.
-	 */
+public interface DataRepositoryTxManager
+{
+    /**
+     * Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns null.
+     * 
+     * @return Returns the current thread's active DynamoDB transaction, if one exists, otherwise returns
+     *         null.
+     */
     Transaction getActiveTransaction();
+
+    <T> T load( Class<T> clazz, Object hashKey, IsolationLevel isolationLevel );
+
+    <T> T load(
+        Class<T> clazz,
+        Object hashKey,
+        boolean isStronglyConsistent,
+        IsolationLevel isolationLevel );
+
+    <T> T load( Class<T> clazz, Object hashKey, Object rangeKey, IsolationLevel isolationLevel );
+
+    <T> T load(
+        Class<T> clazz,
+        Object hashKey,
+        Object rangeKey,
+        boolean isStronglyConsistent,
+        IsolationLevel isolationLevel );
+
+    <T> T load( T item, IsolationLevel isolationLevel );
+
+    <T> T load( T item, boolean isStronglyConsistent, IsolationLevel isolationLevel );
 }
