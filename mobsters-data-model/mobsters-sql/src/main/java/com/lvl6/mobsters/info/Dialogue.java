39d7c6b8d14a41b4bf2adbd769ee920e4d633f34
John Heinnickel <john@lvl6.com>
2014-06-06 23:36:14 -0700

    Formatting only changes

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/Dialogue.java
+++ mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/Dialogue.java
@@ -9,57 +9,76 @@ import javax.persistence.FetchType;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="dialogue")
-public class Dialogue extends BasePersistentObject{
+@Table(
+	name = "dialogue")
+public class Dialogue extends BasePersistentObject
+{
 
-	
-	private static final long serialVersionUID = -8118069262824842364L;  
-	
-	
-	@ElementCollection(fetch=FetchType.EAGER)
-	@Column(name = "is_left_sides")
+	private static final long serialVersionUID = -8118069262824842364L;
+
+	@ElementCollection(
+		fetch = FetchType.EAGER)
+	@Column(
+		name = "is_left_sides")
 	List<Boolean> isLeftSides;
-	
-	@ElementCollection(fetch=FetchType.EAGER)
-	@Column(name = "speakers")
+
+	@ElementCollection(
+		fetch = FetchType.EAGER)
+	@Column(
+		name = "speakers")
 	List<String> speakers;
-	
-	@ElementCollection(fetch=FetchType.EAGER)
-	@Column(name = "speaker_images")
+
+	@ElementCollection(
+		fetch = FetchType.EAGER)
+	@Column(
+		name = "speaker_images")
 	List<String> speakerImages;
-	
-	@ElementCollection(fetch=FetchType.EAGER)
-	@Column(name = "speaker_texts")
+
+	@ElementCollection(
+		fetch = FetchType.EAGER)
+	@Column(
+		name = "speaker_texts")
 	List<String> speakerTexts;
 
-	public Dialogue(){}
-	public Dialogue(List<String> speakers, List<String> speakerImages, List<String> speakerTexts, List<Boolean> isLeftSides) {
+	public Dialogue()
+	{}
+
+	public Dialogue(
+		final List<String> speakers,
+		final List<String> speakerImages,
+		final List<String> speakerTexts,
+		final List<Boolean> isLeftSides )
+	{
 		this.speakers = speakers;
 		this.speakerTexts = speakerTexts;
 		this.speakerImages = speakerImages;
 		this.isLeftSides = isLeftSides;
 	}
 
-  public List<Boolean> getIsLeftSides() {
-    return isLeftSides;
-  }
+	public List<Boolean> getIsLeftSides()
+	{
+		return isLeftSides;
+	}
 
-  public List<String> getSpeakers() {
-    return speakers;
-  }
+	public List<String> getSpeakers()
+	{
+		return speakers;
+	}
 
-  public List<String> getSpeakerImages() {
-    return speakerImages;
-  }
+	public List<String> getSpeakerImages()
+	{
+		return speakerImages;
+	}
 
-	public List<String> getSpeakerTexts() {
+	public List<String> getSpeakerTexts()
+	{
 		return speakerTexts;
 	}
 
 	@Override
-	public String toString() {
-		return "Dialogue [speakers=" + speakers + ", speakerTexts="
-				+ speakerTexts + "]";
+	public String toString()
+	{
+		return "Dialogue [speakers=" + speakers + ", speakerTexts=" + speakerTexts + "]";
 	}
 
 }
