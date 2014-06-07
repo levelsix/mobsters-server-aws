a3ef9fad13a787dd046e3f7c1af2d3b07022dd39
John Heinnickel <john@lvl6.com>
2014-06-06 23:35:49 -0700

    Using load(Object, Object in MonsterFOrUserServiceImpl.
    Use the dog food.

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepositoryImpl.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.lvl6.mobsters.dynamo.repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import com.lvl6.mobsters.dynamo.MonsterForUser;
+
 @Component
 public class MonsterForUserRepositoryImpl extends BaseDynamoRepository<MonsterForUser>
     implements
@@ -16,18 +18,21 @@ public class MonsterForUserRepositoryImpl extends BaseDynamoRepository<MonsterFo
             MonsterForUser.class);
     }
 
-	@Override
-	public List<MonsterForUser> findByUserIdAndId(
-		String userId, Iterable<String> monsterIds) {
-		final ArrayList<MonsterForUser> retVal = new ArrayList<MonsterForUser>();
-		// final Builder retVal = ImmutableList.builder();
-		for (final String monsterForUserId : monsterIds ) {
-			final MonsterForUser nextMonster = load(monsterForUserId);
-			if (userId.equals(nextMonster.getUserId())) {
-				retVal.add(nextMonster);
-			}
-		}
-		return retVal;
-	}
+    @Override
+    public List<MonsterForUser> findByUserIdAndId(
+        final String userId,
+        final Iterable<String> monsterIds )
+    {
+        final ArrayList<MonsterForUser> retVal = new ArrayList<MonsterForUser>();
+        // TODO
+        // final Builder retVal = ImmutableList.builder();
+        for (final String monsterForUserId : monsterIds) {
+            retVal.add(
+	        load(
+                    userId,
+                    monsterForUserId));
+        }
+        return retVal;
+    }
 
 }
