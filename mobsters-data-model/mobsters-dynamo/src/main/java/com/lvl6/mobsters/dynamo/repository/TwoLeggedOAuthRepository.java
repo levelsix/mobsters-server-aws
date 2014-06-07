79465cbfc584da407e92d56f73963e311422990d
John Heinnickel <john@lvl6.com>
2014-06-06 23:27:54 -0700

    Disabled TwoLeggedOAuthRepository
    The entity class for this repository is not properly annotated and Art
    didn't think this is a non-config entity, but wasn't 100% sure.  Rather
    than deleting it, I am merely removing the @Component annotation so it
    won't get pulled in to the list for table management.

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/TwoLeggedOAuthRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/TwoLeggedOAuthRepository.java
@@ -1,8 +1,9 @@
 package com.lvl6.mobsters.dynamo.repository;
-import org.springframework.stereotype.Component;
 
 import com.lvl6.mobsters.dynamo.TwoLeggedOAuth;
-@Component public class TwoLeggedOAuthRepository extends BaseDynamoRepository<TwoLeggedOAuth>{
+
+public class TwoLeggedOAuthRepository extends BaseDynamoRepository<TwoLeggedOAuth>
+{
 	public TwoLeggedOAuthRepository()
 	{
 		super(
