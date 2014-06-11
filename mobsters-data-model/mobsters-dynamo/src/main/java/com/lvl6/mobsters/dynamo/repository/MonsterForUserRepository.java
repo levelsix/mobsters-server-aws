ccba25d3d037815f2091690f18eb58e3f309194f
John Heinnickel <john@lvl6.com>
2014-06-10 17:41:33 -0700

    Interface restoration

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepository.java
@@ -1,102 +1,20 @@
 package com.lvl6.mobsters.dynamo.repository;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
-import com.amazonaws.services.dynamodbv2.model.Condition;
-import com.amazonaws.services.dynamodbv2.model.ConditionalOperator;
 import com.lvl6.mobsters.dynamo.MonsterForUser;
 
 @Component
-public class MonsterForUserRepository extends
-BaseDynamoRepository<MonsterForUser> {
-    
-    private static final Logger log = LoggerFactory
-        .getLogger(MonsterForUserRepository.class);
+public interface MonsterForUserRepository
+{
+	List<MonsterForUser> findByUserIdAndId( String userId, Collection<String> monsterForUserIds );
 
-    protected MonsterForUserRepository()
-    {
-        super(MonsterForUser.class);
-        isActive = true;// for unit test
-    }
-
-    public List<MonsterForUser> findByUserIdAndId(
-        final String userId,
-        final Collection<String> monsterForUserIds )
-    {
-        final List<AttributeValue> monsterForUserIdz = new ArrayList<>();
-        final MonsterForUser hashKey = new MonsterForUser();
-        hashKey.setUserId(userId);
-        for (final String monsterForUserId : monsterForUserIds) {
-            monsterForUserIdz.add(new AttributeValue().withS(monsterForUserId.toString()));
-        }
-
-        final DynamoDBQueryExpression<MonsterForUser> query = new DynamoDBQueryExpression<MonsterForUser>()
-            // .withIndexName("userIdGlobalIndex")
-            .withHashKeyValues(hashKey)
-            .withQueryFilterEntry(
-                "monsterForUserId",
-                new Condition().withComparisonOperator(
-                    ComparisonOperator.IN).withAttributeValueList(
-                        monsterForUserIdz)).withConsistentRead(true);
-
-        MonsterForUserRepository.log.info("Query: {}", query);
-        final PaginatedQueryList<MonsterForUser> monsterForUsersForUser = query(query);
-        monsterForUsersForUser.loadAllResults();
-        return monsterForUsersForUser;
-    }
-    
-    public Map<String, MonsterForUser> findByUserIdAndIdOrTeamSlotNum (
-        final String userId,
-        Collection<String> monsterForUserIds,
-        Integer teamSlotNum)
-   {
-        final List<AttributeValue> monsterForUserIdz = new ArrayList<>();
-        for (final String monsterForUserId : monsterForUserIds) {
-            monsterForUserIdz.add(new AttributeValue().withS(monsterForUserId.toString()));
-        }
-        
-        final MonsterForUser hashKey = new MonsterForUser();
-        hashKey.setUserId(userId);
-
-        final DynamoDBQueryExpression<MonsterForUser> query = new DynamoDBQueryExpression<MonsterForUser>()
-            // .withIndexName("userIdGlobalIndex")
-            .withHashKeyValues(hashKey)
-            .withConditionalOperator(ConditionalOperator.OR)
-            .withQueryFilterEntry(
-                "monsterForUserId",
-                new Condition().withComparisonOperator(
-                    ComparisonOperator.IN).withAttributeValueList(
-                        monsterForUserIdz))
-            .withQueryFilterEntry(
-                "teamSlotNum",
-                new Condition().withComparisonOperator(
-                    ComparisonOperator.EQ).withAttributeValueList(
-                        new AttributeValue().withN(
-                            teamSlotNum.toString()))).withConsistentRead(true);
-
-        MonsterForUserRepository.log.info("Query: {}", query);
-        final PaginatedQueryList<MonsterForUser> monstersForUser = query(query);
-        monstersForUser.loadAllResults();
-        
-        
-        Map<String, MonsterForUser> userMonsterIdToMfu = new HashMap<String, MonsterForUser>();
-        
-        for (MonsterForUser mfu : monstersForUser) {
-            userMonsterIdToMfu.put(mfu.getMonsterForUserId(), mfu);
-        }
-        return userMonsterIdToMfu;
-    }
-    
+	Map<String, MonsterForUser> findByUserIdAndIdOrTeamSlotNum(
+		String userId,
+		Collection<String> monsterForUserIds,
+		Integer teamSlotNum );
 }
--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepositoryImpl.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterForUserRepositoryImpl.java
@@ -1,38 +1,104 @@
 package com.lvl6.mobsters.dynamo.repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.ConditionalOperator;
 import com.lvl6.mobsters.dynamo.MonsterForUser;
 
 @Component
 public class MonsterForUserRepositoryImpl extends BaseDynamoRepository<MonsterForUser>
-    implements
-        MonsterForUserRepository
+	implements MonsterForUserRepository
 {
-    public MonsterForUserRepositoryImpl()
-    {
-        super(
-            MonsterForUser.class);
-    }
 
-    @Override
-    public List<MonsterForUser> findByUserIdAndId(
-        final String userId,
-        final Iterable<String> monsterIds )
-    {
-        final ArrayList<MonsterForUser> retVal = new ArrayList<MonsterForUser>();
-        // TODO
-        // final Builder retVal = ImmutableList.builder();
-        for (final String monsterForUserId : monsterIds) {
-            retVal.add(
-	        load(
-                    userId,
-                    monsterForUserId));
-        }
-        return retVal;
-    }
+	private static final Logger log =
+		LoggerFactory.getLogger(MonsterForUserRepositoryImpl.class);
+
+	protected MonsterForUserRepositoryImpl()
+	{
+		super(MonsterForUser.class);
+		isActive = true;// for unit test
+	}
+
+	@Override
+	public List<MonsterForUser> findByUserIdAndId(
+		final String userId,
+		final Collection<String> monsterForUserIds )
+	{
+		final List<AttributeValue> monsterForUserIdz = new ArrayList<>();
+		final MonsterForUser hashKey = new MonsterForUser();
+		hashKey.setUserId(userId);
+		for (final String monsterForUserId : monsterForUserIds) {
+			monsterForUserIdz.add(new AttributeValue().withS(monsterForUserId.toString()));
+		}
+
+		final DynamoDBQueryExpression<MonsterForUser> query =
+			new DynamoDBQueryExpression<MonsterForUser>()
+			// .withIndexName("userIdGlobalIndex")
+			.withHashKeyValues(hashKey)
+				.withQueryFilterEntry("monsterForUserId",
+					new Condition().withComparisonOperator(ComparisonOperator.IN)
+						.withAttributeValueList(monsterForUserIdz))
+				.withConsistentRead(true);
+
+		log.info("Query: {}", query);
+		final PaginatedQueryList<MonsterForUser> monsterForUsersForUser = query(query);
+		monsterForUsersForUser.loadAllResults();
+		return monsterForUsersForUser;
+	}
+
+	@Override
+	public Map<String, MonsterForUser> findByUserIdAndIdOrTeamSlotNum(
+		final String userId,
+		final Collection<String> monsterForUserIds,
+		final Integer teamSlotNum )
+	{
+		final List<AttributeValue> monsterForUserIdz = new ArrayList<>();
+		for (final String monsterForUserId : monsterForUserIds) {
+			monsterForUserIdz.add(new AttributeValue().withS(monsterForUserId.toString()));
+		}
+
+		final MonsterForUser hashKey = new MonsterForUser();
+		hashKey.setUserId(userId);
+
+		final DynamoDBQueryExpression<MonsterForUser> query =
+			new DynamoDBQueryExpression<MonsterForUser>()
+			// .withIndexName("userIdGlobalIndex")
+			.withHashKeyValues(hashKey)
+				.withConditionalOperator(ConditionalOperator.OR)
+				.withQueryFilterEntry("monsterForUserId",
+					new Condition().withComparisonOperator(ComparisonOperator.IN)
+						.withAttributeValueList(monsterForUserIdz))
+				.withQueryFilterEntry(
+					"teamSlotNum",
+					new Condition().withComparisonOperator(ComparisonOperator.EQ)
+						.withAttributeValueList(
+							new AttributeValue().withN(teamSlotNum.toString())))
+				.withConsistentRead(true);
+
+		MonsterForUserRepository.log.info("Query: {}", query);
+		final PaginatedQueryList<MonsterForUser> monstersForUser = query(query);
+		monstersForUser.loadAllResults();
+
+		final Map<String, MonsterForUser> userMonsterIdToMfu =
+			new HashMap<String, MonsterForUser>();
+
+		for (final MonsterForUser mfu : monstersForUser) {
+			userMonsterIdToMfu.put(mfu.getMonsterForUserId(), mfu);
+		}
+		return userMonsterIdToMfu;
+	}
 
 }
