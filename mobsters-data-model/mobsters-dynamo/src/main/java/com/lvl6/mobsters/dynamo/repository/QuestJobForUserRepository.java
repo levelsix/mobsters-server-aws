acacc908e44a07a02292107e026f1bc32879aebb
Arturo Wu Zhou <aweezy90@gmail.com>
2014-06-16 18:15:05 -0700

    formatting

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/QuestJobForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/QuestJobForUserRepository.java
@@ -1,9 +1,14 @@
 package com.lvl6.mobsters.dynamo.repository;
+
 import org.springframework.stereotype.Component;
 
 import com.lvl6.mobsters.dynamo.QuestJobForUser;
-@Component public class QuestJobForUserRepository extends BaseDynamoRepositoryImpl<QuestJobForUser>{
-	public QuestJobForUserRepository(){
+
+@Component
+public class QuestJobForUserRepository extends BaseDynamoRepositoryImpl<QuestJobForUser>
+{
+	public QuestJobForUserRepository()
+	{
 		super(QuestJobForUser.class);
 	}
 
