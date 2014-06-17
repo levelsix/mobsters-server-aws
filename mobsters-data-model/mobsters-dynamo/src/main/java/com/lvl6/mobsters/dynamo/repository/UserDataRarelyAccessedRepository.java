0210be3bf92d503646db2466f73870dbfc18da5a
Arturo Wu Zhou <aweezy90@gmail.com>
2014-06-16 22:06:44 -0700

    created logger with incorrect class type

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserDataRarelyAccessedRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserDataRarelyAccessedRepository.java
@@ -18,7 +18,7 @@ public class UserDataRarelyAccessedRepository
 
 	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory
-			.getLogger(QuestForUserRepository.class);
+			.getLogger(UserDataRarelyAccessedRepository.class);
 
 	public UserDataRarelyAccessedRepository() {
 		super(UserDataRarelyAccessed.class);
