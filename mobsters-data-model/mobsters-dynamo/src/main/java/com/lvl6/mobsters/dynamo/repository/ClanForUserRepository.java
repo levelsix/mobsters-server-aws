bb2e8cbb9a966e46e091f1839c2d7d0e7756467b
Arturo Wu Zhou <aweezy90@gmail.com>
2014-06-18 00:58:18 -0700

    fixed up ClanForUser schema and method to find all for a userId

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ClanForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ClanForUserRepository.java
@@ -2,7 +2,10 @@ package com.lvl6.mobsters.dynamo.repository;
 
 import java.util.List;
 
+import com.lvl6.mobsters.dynamo.ClanForUser;
+
 public interface ClanForUserRepository
 {
 	public List<ClanForUser> findByUserId( String userId );
+	
 }
--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ClanForUserRepositoryImpl.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ClanForUserRepositoryImpl.java
@@ -1,10 +1,39 @@
 package com.lvl6.mobsters.dynamo.repository;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.lvl6.mobsters.dynamo.ClanForUser;
 @Component public class ClanForUserRepositoryImpl extends BaseDynamoRepositoryImpl<ClanForUser> implements ClanForUserRepository{
+	
+	private static final Logger log = LoggerFactory
+		.getLogger(ClanForUserRepositoryImpl.class);
+	
 	public ClanForUserRepositoryImpl(){
 		super(ClanForUser.class);
+	}
+
+	@Override
+	public List<ClanForUser> findByUserId( String userId )
+	{
+		final ClanForUser hashKey = new ClanForUser();
+		hashKey.setUserId(userId);
+		
+		final DynamoDBQueryExpression<ClanForUser> query =
+			new DynamoDBQueryExpression<ClanForUser>()
+			//.withIndexName("userIdGlobalIndex")
+			.withHashKeyValues(hashKey)
+			.withConsistentRead(true);
+		log.info(
+			"Query: {}",
+			query);
+		final PaginatedQueryList<ClanForUser> clansForUser = query(query);
+		clansForUser.loadAllResults();
+		return clansForUser;
 	}
  
 }
