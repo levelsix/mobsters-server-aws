e72465d1abb4ee0aab20fc74a8393509658e5350
Arturo Wu Zhou <aweezy90@gmail.com>
2014-06-24 19:24:49 -0700

    initial examples of Interfaces on entities and having actual Entity properties of other classes instead of integer id properties of other classes

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/BoosterItem.java
+++ mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/BoosterItem.java
@@ -1,36 +1,71 @@
 package com.lvl6.mobsters.info;
 
 import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Proxy;
+
+//monster_id, gems, and cash are all mutually exclusive
 
 @Entity
-public class BoosterItem extends BaseIntPersistentObject{	
+@Table(name="booster_item")
+//created proxy because don't want hibernate to create two instances of this class (?)
+@Proxy(lazy=true, proxyClass=IBoosterItem.class) 
+public class BoosterItem extends BaseIntPersistentObject implements IBoosterItem{	
+
+	private static final long serialVersionUID = -5013736350979058326L;
 
 	
-	private static final long serialVersionUID = -4823600133447078457L;
-	@Column(name = "booster_pack_id")
-	private int boosterPackId;
-	@Column(name = "monster_id")
-	private int monsterId;
+	//"targetEntity=BoosterPack.class" is specified because the property is IBoosterPack
+	//and need a real implementation
+	@ManyToOne(fetch=FetchType.EAGER, targetEntity=BoosterPack.class)
+	@JoinColumn(
+		name = "booster_pack_id", //exact column name (verbatim) in the booster_item table 
+		nullable = false, //(foreign key must be valid) a booster item must have a booster pack
+		//name=none means that hibernate won't create a foreign key constraint in the db
+		//NO_CONSTRAINT flag is not used, more of an explanation for "name=none"
+		foreignKey = @ForeignKey(name="none", value=ConstraintMode.NO_CONSTRAINT)) 
+	private IBoosterPack boosterPack;
+	
+	//"optional=true" means monster object/reference can be nonexistent/null
+	@ManyToOne(fetch=FetchType.LAZY, targetEntity=Monster.class, optional=true)
+	@JoinColumn(
+		name = "monster_id",
+		nullable = true,
+		foreignKey = @ForeignKey(name="none", value=ConstraintMode.NO_CONSTRAINT))
+	private Monster monster;
+	
 	@Column(name = "num_pieces")
 	private int numPieces;
+	
 	@Column(name = "is_complete")
 	private boolean isComplete;
+	
 	@Column(name = "is_special")
 	private boolean isSpecial;
+	
 	@Column(name = "gem_reward")
 	private int gemReward;
+	
 	@Column(name = "cash_reward")
 	private int cashReward;
+	
 	@Column(name = "chance_to_appear")
 	private float chanceToAppear;  
+	
 	public BoosterItem(){}
-	public BoosterItem(int id, int boosterPackId, int monsterId, int numPieces,
+	public BoosterItem(int id, IBoosterPack boosterPack, Monster monster, int numPieces,
 			boolean isComplete, boolean isSpecial, int gemReward, int cashReward,
 			float chanceToAppear) {
 		super(id);
-		this.boosterPackId = boosterPackId;
-		this.monsterId = monsterId;
+		this.boosterPack = boosterPack;
+		this.monster = monster;
 		this.numPieces = numPieces;
 		this.isComplete = isComplete;
 		this.isSpecial = isSpecial;
@@ -41,74 +76,138 @@ public class BoosterItem extends BaseIntPersistentObject{
 
 
 
-	public int getBoosterPackId() {
-		return boosterPackId;
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getBoosterPack()
+	 */
+	@Override
+	public IBoosterPack getBoosterPack() {
+		return boosterPack;
 	}
 
-	public void setBoosterPackId(int boosterPackId) {
-		this.boosterPackId = boosterPackId;
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setBoosterPack(com.lvl6.mobsters.info.BoosterPack)
+	 */
+	@Override
+	public void setBoosterPack(IBoosterPack boosterPack) {
+		this.boosterPack = boosterPack;
 	}
 
-	public int getMonsterId() {
-		return monsterId;
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getMonster()
+	 */
+	@Override
+	public Monster getMonster() {
+		return monster;
 	}
 
-	public void setMonsterId(int monsterId) {
-		this.monsterId = monsterId;
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setMonster(com.lvl6.mobsters.info.Monster)
+	 */
+	@Override
+	public void setMonster(Monster monster) {
+		this.monster = monster;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getNumPieces()
+	 */
+	@Override
 	public int getNumPieces() {
 		return numPieces;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setNumPieces(int)
+	 */
+	@Override
 	public void setNumPieces(int numPieces) {
 		this.numPieces = numPieces;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#isComplete()
+	 */
+	@Override
 	public boolean isComplete() {
 		return isComplete;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setComplete(boolean)
+	 */
+	@Override
 	public void setComplete(boolean isComplete) {
 		this.isComplete = isComplete;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#isSpecial()
+	 */
+	@Override
 	public boolean isSpecial() {
 		return isSpecial;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setSpecial(boolean)
+	 */
+	@Override
 	public void setSpecial(boolean isSpecial) {
 		this.isSpecial = isSpecial;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getGemReward()
+	 */
+	@Override
 	public int getGemReward() {
 		return gemReward;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setGemReward(int)
+	 */
+	@Override
 	public void setGemReward(int gemReward) {
 		this.gemReward = gemReward;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getCashReward()
+	 */
+	@Override
 	public int getCashReward() {
 		return cashReward;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setCashReward(int)
+	 */
+	@Override
 	public void setCashReward(int cashReward) {
 		this.cashReward = cashReward;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#getChanceToAppear()
+	 */
+	@Override
 	public float getChanceToAppear() {
 		return chanceToAppear;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.lvl6.mobsters.info.IBoosterItem#setChanceToAppear(float)
+	 */
+	@Override
 	public void setChanceToAppear(float chanceToAppear) {
 		this.chanceToAppear = chanceToAppear;
 	}
 
 	@Override
 	public String toString() {
-		return "BoosterItem [id=" + id + ", boosterPackId=" + boosterPackId
-				+ ", monsterId=" + monsterId + ", numPieces=" + numPieces
+		return "BoosterItem [id=" + id + ", boosterPack=" + boosterPack
+				+ ", monster=" + monster + ", numPieces=" + numPieces
 				+ ", isComplete=" + isComplete + ", isSpecial=" + isSpecial
 				+ ", gemReward=" + gemReward + ", cashReward=" + cashReward
 				+ ", chanceToAppear=" + chanceToAppear + "]";
--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/BoosterPack.java
+++ mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/BoosterPack.java
@@ -1,13 +1,24 @@
 package com.lvl6.mobsters.info;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Proxy;
 
 @Entity
-public class BoosterPack extends BaseIntPersistentObject{	
+@Table(name="booster_pack")
+@Proxy(lazy=false, proxyClass=IBoosterPack.class)
+public class BoosterPack extends BaseIntPersistentObject implements IBoosterPack{	
 
+	private static final long serialVersionUID = -7927101098350853116L;
 	
-	private static final long serialVersionUID = -3541561961941269992L;
 	@Column(name = "name")
 	private String name;
 	@Column(name = "gem_price")
@@ -21,11 +32,21 @@ public class BoosterPack extends BaseIntPersistentObject{
 	@Column(name = "nav_title_img_name")
 	private String navTitleImgName;
 	@Column(name = "machine_img_name")
-	private String machineImgName;  
+	private String machineImgName;
+	
+	@OneToMany(
+		cascade={CascadeType.PERSIST, CascadeType.REFRESH},
+		fetch=FetchType.EAGER,
+		mappedBy="boosterPack", //the exact property name (verbatim) in BoosterItem.java 
+		orphanRemoval=true,
+		targetEntity=BoosterItem.class)
+	private List<IBoosterItem> packItems;  
+	
 	public BoosterPack(){}
 	public BoosterPack(int id, String name, int gemPrice,
 			String listBackgroundImgName, String listDescription,
-			String navBarImgName, String navTitleImgName, String machineImgName) {
+			String navBarImgName, String navTitleImgName, String machineImgName,
+			List<BoosterItem> packItems) {
 		super(id);
 		this.name = name;
 		this.gemPrice = gemPrice;
@@ -34,66 +55,92 @@ public class BoosterPack extends BaseIntPersistentObject{
 		this.navBarImgName = navBarImgName;
 		this.navTitleImgName = navTitleImgName;
 		this.machineImgName = machineImgName;
+		this.setPackItems(new ArrayList<IBoosterItem>(packItems));
 	}
 
 
 
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	@Override
 	public int getGemPrice() {
 		return gemPrice;
 	}
 
+	@Override
 	public void setGemPrice(int gemPrice) {
 		this.gemPrice = gemPrice;
 	}
 
+	@Override
 	public String getListBackgroundImgName() {
 		return listBackgroundImgName;
 	}
 
+	@Override
 	public void setListBackgroundImgName(String listBackgroundImgName) {
 		this.listBackgroundImgName = listBackgroundImgName;
 	}
 
+	@Override
 	public String getListDescription() {
 		return listDescription;
 	}
 
+	@Override
 	public void setListDescription(String listDescription) {
 		this.listDescription = listDescription;
 	}
 
+	@Override
 	public String getNavBarImgName() {
 		return navBarImgName;
 	}
 
+	@Override
 	public void setNavBarImgName(String navBarImgName) {
 		this.navBarImgName = navBarImgName;
 	}
 
+	@Override
 	public String getNavTitleImgName() {
 		return navTitleImgName;
 	}
 
+	@Override
 	public void setNavTitleImgName(String navTitleImgName) {
 		this.navTitleImgName = navTitleImgName;
 	}
 
+	@Override
 	public String getMachineImgName() {
 		return machineImgName;
 	}
 
+	@Override
 	public void setMachineImgName(String machineImgName) {
 		this.machineImgName = machineImgName;
 	}
 
+	@Override
+	public List<IBoosterItem> getPackItems()
+	{
+		return packItems;
+	}
+	@Override
+	public void setPackItems( List<IBoosterItem> packItems )
+	{
+		this.packItems = packItems;
+	}
+	
 	@Override
 	public String toString() {
 		return "BoosterPack [id=" + id + ", name=" + name + ", gemPrice="
