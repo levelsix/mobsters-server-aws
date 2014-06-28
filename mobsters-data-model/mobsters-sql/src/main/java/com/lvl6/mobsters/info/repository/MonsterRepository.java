2971a9126e4514116d711f9efb4c5eeb6ed97042
John Heinnickel <john@lvl6.com>
2014-06-28 02:13:41 -0700

    Corrected MonsterRepository config repository to match the model's id type.

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/MonsterRepository.java
+++ mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/MonsterRepository.java
@@ -2,6 +2,6 @@ package com.lvl6.mobsters.info.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lvl6.mobsters.info.Monster;
-public interface MonsterRepository extends JpaRepository<Monster, String>{
+public interface MonsterRepository extends JpaRepository<Monster, Integer>{
 
 }
