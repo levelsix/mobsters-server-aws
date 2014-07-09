4f2feeca933992ecc31d5b8b94ceec2a276e2947
John Heinnickel <john@lvl6.com>
2014-07-09 08:56:12 -0700

    Reactivated UserDataRarelyAccessedRepository
    Still need to factor out an interface...

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserDataRarelyAccessedRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserDataRarelyAccessedRepository.java
@@ -12,53 +12,23 @@ import com.amazonaws.services.dynamodbv2.model.LocalSecondaryIndex;
 import com.lvl6.mobsters.dynamo.UserDataRarelyAccessed;
 
 @Component
-public abstract class UserDataRarelyAccessedRepository
+public class UserDataRarelyAccessedRepository
 	extends BaseDynamoItemRepositoryImpl<UserDataRarelyAccessed>
 {
-
 	@SuppressWarnings("unused")
-	private static final Logger log = LoggerFactory
+	private static final Logger LOG = LoggerFactory
 			.getLogger(UserDataRarelyAccessedRepository.class);
 
 	public UserDataRarelyAccessedRepository() {
 		super(UserDataRarelyAccessed.class);
 	}
 
-	/*
 	@Override
-	protected void createTable() {
-		try {
-			log.info("Creating Dynamo table {}", getTableName());
-			ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
-			attributeDefinitions.add(new AttributeDefinition()
-					.withAttributeName("userId").withAttributeType("S"));
-
-			ArrayList<KeySchemaElement> ks = new ArrayList<KeySchemaElement>();
-			ks.add(new KeySchemaElement().withAttributeName("userId")
-					.withKeyType(KeyType.HASH));
-
-			CreateTableRequest request = new CreateTableRequest()
-					.withTableName(getTableName())
-					.withAttributeDefinitions(attributeDefinitions)
-					.withKeySchema(ks)
-					.withProvisionedThroughput(provisionedThroughput);
-			List<GlobalSecondaryIndex> globalIndexes = getGlobalIndexes();
-			if (globalIndexes != null && !globalIndexes.isEmpty()) {
-				request.withGlobalSecondaryIndexes(globalIndexes);
-			}
-			List<LocalSecondaryIndex> localIndexes = getLocalIndexes();
-			if (localIndexes != null && !localIndexes.isEmpty()) {
-				request.withLocalSecondaryIndexes(localIndexes);
-			}
-			log.info("Creating table: {}", request);
-			CreateTableResult result = client.createTable(request);
-			log.info("Create table result: {}", result);
-		} catch (Throwable e) {
-			log.error("Error creating Dynamo table {}", getTableName(), e);
-			throw e;
-		}
+	protected UserDataRarelyAccessed getHashKeyObject(final String hashKey) {
+		final UserDataRarelyAccessed retVal = new UserDataRarelyAccessed();
+		retVal.setUserId(hashKey);
+		return retVal;
 	}
-	*/
 
 	@Override
 	public List<LocalSecondaryIndex> getLocalIndexes() {
@@ -69,5 +39,4 @@ public abstract class UserDataRarelyAccessedRepository
 	public List<GlobalSecondaryIndex> getGlobalIndexes() {
 		return Collections.emptyList();
 	}
-
 }
