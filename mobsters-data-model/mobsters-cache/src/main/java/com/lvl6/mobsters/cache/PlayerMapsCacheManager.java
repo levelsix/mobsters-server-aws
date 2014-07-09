d5c4c77f03805dcbe60007a7ac4ba4dbc5901ae3
John Heinnickel <john@lvl6.com>
2014-07-09 06:12:24 -0700

    Using methods for applying key suffix strings to ConnectedPlayer cache methods
    Two methods for appending a key type suffix to either a Udid (pre-DB
    connection) or PlayerId (post-DB connection) were previously added, but
    the step of actually using them to create key namespaces was forgotten.

--- mobsters-data-model/mobsters-cache/src/main/java/com/lvl6/mobsters/cache/PlayerMapsCacheManager.java
+++ mobsters-data-model/mobsters-cache/src/main/java/com/lvl6/mobsters/cache/PlayerMapsCacheManager.java
@@ -15,38 +15,49 @@ public class PlayerMapsCacheManager extends AbstractCacheManager<ConnectedPlayer
 	protected static String connectedPlayersPreDB = "connectedPlayersPreDB";
 	protected static String connectedPlayers = "connectedPlayers";
 	
+	private static int PREDB_KEY_BASELEN = connectedPlayersPreDB.length() + 1;
+	private static int POSTDB_KEY_BASELEN = connectedPlayers.length() + 1;
+	
 	protected static Long connectedPlayerTimeout = 12*60*60l;
 	protected static Long connectedPlayerPreDBTimeout = 60*60l;
 	
 	
 	
 	public ConnectedPlayer getPlayer(String userId) {
-		return getEntity(userId);
+		return getEntity(connectedPlayerKey(userId));
 	}
 	
 	
-	public ConnectedPlayer getPlayerPreDB(String uuid) {
-		return getEntity(uuid);
+	public ConnectedPlayer getPlayerPreDB(String udid) {
+		return getEntity(connectedPlayerPreDBKey(udid));
 	}
 	
 	
 	public void savePlayer(ConnectedPlayer player) {
-		saveEntity(player, player.getPlayerId(), connectedPlayerTimeout);
+		saveEntity(player, connectedPlayerKey(player.getPlayerId()), connectedPlayerTimeout);
 	}
 	
 	public void savePlayerPreDB(ConnectedPlayer player) {
-		saveEntity(player, player.getUdid(), connectedPlayerPreDBTimeout);
+		saveEntity(player, connectedPlayerPreDBKey(player.getUdid()), connectedPlayerPreDBTimeout);
 	}
 	
-	
-	
-	protected String connectedPlayerKey(String userId) {
-		return new StringBuffer().append(userId).append(":").append(connectedPlayers).toString();
+	private String connectedPlayerKey(String userId) {
+		return 
+			new StringBuffer(
+				userId.length() + POSTDB_KEY_BASELEN)
+			.append(userId)
+			.append(":")
+			.append(connectedPlayers)
+			.toString();
 	}
 	
-	protected String connectedPlayerPreDBKey(String uuid) {
-		return new StringBuffer().append(uuid).append(":").append(connectedPlayersPreDB).toString();
+	private String connectedPlayerPreDBKey(String uuid) {
+		return 
+			new StringBuffer(
+				uuid.length() + PREDB_KEY_BASELEN)
+			.append(uuid)
+			.append(":")
+			.append(connectedPlayersPreDB)
+			.toString();
 	}
-	
-	
 }
