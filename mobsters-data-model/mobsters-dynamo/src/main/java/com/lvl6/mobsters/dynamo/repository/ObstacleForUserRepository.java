a2f5a041dde489043b2a0c8f571962da7540021e
John Heinnickel <john@lvl6.com>
2014-07-09 06:38:20 -0700

    Reactivating ObstacleForUserRepository as a Collection repo

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ObstacleForUserRepository.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/ObstacleForUserRepository.java
@@ -1,10 +1,6 @@
 package com.lvl6.mobsters.dynamo.repository;
-import org.springframework.stereotype.Component;
-
 import com.lvl6.mobsters.dynamo.ObstacleForUser;
-@Component public abstract class ObstacleForUserRepository extends BaseDynamoItemRepositoryImpl<ObstacleForUser>{
-	public ObstacleForUserRepository(){
-		super(ObstacleForUser.class);
-	}
 
+public interface ObstacleForUserRepository extends BaseDynamoCollectionRepository<ObstacleForUser, String>
+{
 }
