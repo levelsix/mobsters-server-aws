bba33577e80d2fe7df05d30364aa171017002405
John Heinnickel <john@lvl6.com>
2014-07-16 08:27:07 -0700

    Support construction of UserCreateResponseEvent as an immutable structure
    Answering the C10K problem in Java involves mitigating the cost of
    increased thread count by releasing threads during asynchoronous work
    and using distinct thread pools for distinct phases of request handling.
    
    Using the Asynchronous Servlet interface for game server scalability
    means that request objects may not be serialized by the same thread that
    created them.  The most efficient way of addressing this design
    constraint is immutability, eliminating the need for memory barriers as
    objects cross thread boundaries.
    
    The ResponseEvent object hierarchy can be made immutable with trivial
    adjustments to how they are constructed, and the base classes are
    already setup to make this retrofit possible incrementally.  This commit
    carries out the final steps for UserCreateResponseEvent.

--- mobsters-functional/mobsters-api-events/src/main/java/com/lvl6/mobsters/events/PreDatabaseResponseEvent.java
+++ mobsters-functional/mobsters-api-events/src/main/java/com/lvl6/mobsters/events/PreDatabaseResponseEvent.java
@@ -1,10 +1,17 @@
 package com.lvl6.mobsters.events;
 
+import com.lvl6.mobsters.noneventproto.ConfigEventProtocolProto.EventProtocolResponse;
+
 public abstract class PreDatabaseResponseEvent extends ResponseEvent{
-  protected String udid;
+  protected final String udid;
   
-  public PreDatabaseResponseEvent(String udid) {
-	super();
+  protected PreDatabaseResponseEvent(final String udid, final EventProtocolResponse eventType) {
+	  super(eventType);
+	  this.udid = udid;
+  }
+  
+  protected PreDatabaseResponseEvent(final String udid, final EventProtocolResponse eventType, final int tag) {
+	super(eventType, tag);
     this.udid = udid;
   }
 
