55faaaa511ac9f54f02f2fe64a9b1140c2ec3b2b
John Heinnickel <john@lvl6.com>
2014-08-07 20:51:55 -0700

    QuestJobMonsterItem lookup method for one monster Id and several questJob ids

--- mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/QuestJobMonsterItemRepository.java
+++ mobsters-data-model/mobsters-sql/src/main/java/com/lvl6/mobsters/info/repository/QuestJobMonsterItemRepository.java
@@ -1,8 +1,13 @@
 package com.lvl6.mobsters.info.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lvl6.mobsters.info.IMonster;
+import com.lvl6.mobsters.info.IQuestJob;
 import com.lvl6.mobsters.info.QuestJobMonsterItem;
-public interface QuestJobMonsterItemRepository extends JpaRepository<QuestJobMonsterItem, Integer>{
-	
-	QuestJobMonsterItem findByQuestJobIdAndMonsterId( int questJobId, int monsterId );
+public interface QuestJobMonsterItemRepository 
+	extends JpaRepository<QuestJobMonsterItem, Integer>
+{
+	List<QuestJobMonsterItem> findByMonsterAndQuestJobIn( IMonster m, Iterable<IQuestJob> qJobs );
 }
