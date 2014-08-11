1a8ae721a0fa6099763aa6f91f76e86d56727323
Arturo Wu Zhou <aweezy90@gmail.com>
2014-08-11 14:52:51 -0700

    fixing up the repository to follow the interface, implementation pattern

---
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserFacebookInviteForSlotRepository.java
@@ -0,0 +1,9 @@
+package com.lvl6.mobsters.dynamo.repository;
+
+import com.lvl6.mobsters.dynamo.UserFacebookInviteForSlot;
+
+public interface UserFacebookInviteForSlotRepository extends
+	BaseDynamoCollectionRepository<UserFacebookInviteForSlot, String>
+{
+	
+}
--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserFacebookInviteForSlotRepositoryImpl.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/UserFacebookInviteForSlotRepositoryImpl.java
@@ -1,10 +1,21 @@
 package com.lvl6.mobsters.dynamo.repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.lvl6.mobsters.dynamo.UserFacebookInviteForSlot;
-@Component public abstract class UserFacebookInviteForSlotRepository extends BaseDynamoItemRepositoryImpl<UserFacebookInviteForSlot>{
-	public UserFacebookInviteForSlotRepository(){
-		super(UserFacebookInviteForSlot.class);
+@Component
+public class UserFacebookInviteForSlotRepositoryImpl extends
+	BaseDynamoCollectionRepositoryImpl<UserFacebookInviteForSlot, String>
+		implements UserFacebookInviteForSlotRepository
+{
+	@SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory
+			.getLogger(UserFacebookInviteForSlotRepositoryImpl.class);
+
+	
+	public UserFacebookInviteForSlotRepositoryImpl(){
+		super( UserFacebookInviteForSlot.class, "recipientFacebookId", String.class );
 	}
 
 }
