9b65af4f98e334d8f41fa5dbdbc72acc47015154
John Heinnickel <john@lvl6.com>
2014-08-26 20:16:40 -0700

    A change that belonged in the formatting and compiler warnings check-in, but mised.

--- mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterEnhancingForUserRepositoryImpl.java
+++ mobsters-data-model/mobsters-dynamo/src/main/java/com/lvl6/mobsters/dynamo/repository/MonsterEnhancingForUserRepositoryImpl.java
@@ -1,28 +1,30 @@
 package com.lvl6.mobsters.dynamo.repository;
-import java.util.List;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.lvl6.mobsters.dynamo.MonsterEnhancingForUser;
-@Component public class MonsterEnhancingForUserRepositoryImpl extends BaseDynamoCollectionRepositoryImpl<MonsterEnhancingForUser, String>
-	implements
-		MonsterEnhancingForUserRepository
+
+
+@Component
+public class MonsterEnhancingForUserRepositoryImpl
+	extends BaseDynamoCollectionRepositoryImpl<MonsterEnhancingForUser, String>
+	implements MonsterEnhancingForUserRepository
 {
+	@SuppressWarnings("unused")
 	private static final Logger LOG =
 		LoggerFactory.getLogger(MonsterEnhancingForUserRepositoryImpl.class);
-	
-	public MonsterEnhancingForUserRepositoryImpl(){
+
+	public MonsterEnhancingForUserRepositoryImpl()
+	{
 		super(MonsterEnhancingForUser.class, "monsterForUserId", String.class);
 	}
-	
+
 	@Override
-	public List<MonsterEnhancingForUser> findByUserId( String userId )
+	public List<MonsterEnhancingForUser> findByUserId(String userId) 
 	{
 		return loadAll(userId);
 	}
-
 }
