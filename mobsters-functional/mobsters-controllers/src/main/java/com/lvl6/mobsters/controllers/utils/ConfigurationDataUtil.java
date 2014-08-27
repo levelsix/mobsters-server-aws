ce42666a5488426aa25a19f634bf34c0f7dbfc43
John Heinnickel <john@lvl6.com>
2014-08-26 22:03:28 -0700

    Java best practices for static utility classes.
    Static utility classes should:
    1) Be final so they cannot be subclassed.
    2) Have a private no-argument constructor to prevent instantiation.

--- mobsters-functional/mobsters-controllers/src/main/java/com/lvl6/mobsters/controllers/utils/ConfigurationDataUtil.java
+++ mobsters-functional/mobsters-controllers/src/main/java/com/lvl6/mobsters/controllers/utils/ConfigurationDataUtil.java
@@ -6,8 +6,13 @@ import java.util.Map;
 
 import com.lvl6.mobsters.info.BaseIntPersistentObject;
 
-public class ConfigurationDataUtil
+// static helper utility class convention #1 -- no sub-classing; declare final.
+public final class ConfigurationDataUtil
 {
+	// Static helper utility class convention #2 -- Only constructor is unused and private.
+	private ConfigurationDataUtil()
+	{}
+	
 	public static Map<Integer, BaseIntPersistentObject> mapifyConfigurationData(Collection<?> data) {
 		Map<Integer, BaseIntPersistentObject> results = new HashMap<Integer, BaseIntPersistentObject>();
 		
