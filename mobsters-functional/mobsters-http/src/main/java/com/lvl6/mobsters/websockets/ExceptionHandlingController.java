4c3fe1b4e482b0a4b33e6d8b1ff2016388746ab8
John Heinnickel <john@lvl6.com>
2014-08-27 01:16:36 -0700

    A possible exception handler.
    Not currently used.  Not certain if this is the intended way
    to capture Exceptions.  Will know more when I attempt to use it.

---
+++ mobsters-functional/mobsters-http/src/main/java/com/lvl6/mobsters/websockets/ExceptionHandlingController.java
@@ -0,0 +1,34 @@
+package com.lvl6.mobsters.websockets;
+
+import java.util.Arrays;
+
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.stereotype.Controller;
+
+import com.lvl6.mobsters.utility.exception.Lvl6MobstersException;
+
+
+@Controller("exceptionHandlingController")
+public class ExceptionHandlingController {
+	private static Logger LOG =
+		LoggerFactory.getLogger(ExceptionHandlingController.class);
+	
+	@MessageExceptionHandler
+	void handleException(final Throwable e) {
+		LOG.error(
+			"Unexpected exception thrown while handling request: %s\n%s",
+			e.getMessage(),
+			IterableExtensions.join(
+				Arrays.asList(e.getStackTrace()), "\n")
+		);
+		
+		if (e instanceof Lvl6MobstersException) {
+			// TODO: Send expected ResponseProto as an event with status code taken from Exception.
+		} else {
+			// TODO: Send expected ResponseProto as an event with status code set to FAIL_OTHER.
+		}
+	}
+}
