b03b16210ce6af97b482cbe41ce8c2427aa5fde9
John Heinnickel <john@lvl6.com>
2014-09-11 20:40:41 -0700

    Enhancement to the first draft LegacyGameServer Client component.
    Replaced "java.io.ByteArrayOutputStream" with "java.nio.ByteBuffer"
    -- Requires no synchronized method calls whereas the previous version
    involved needed five.
    -- Requires allocation of only one byte array for buffering the outbound
    message payload, whereas the first version allocated two, copied the
    first into the second, and released the first buffer on return.
    -- Does not need any checked exception handling or propagation.

--- mobsters-functional/mobsters-http/src/main/java/com/lvl6/mobsters/websocket/adapters/LegacyGameSrvrClient.java
+++ mobsters-functional/mobsters-http/src/main/java/com/lvl6/mobsters/websocket/adapters/LegacyGameSrvrClient.java
@@ -1,25 +1,31 @@
 package com.lvl6.mobsters.websocket.adapters;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompConversionException;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 
 import com.lvl6.mobsters.websockets.MobstersHeaderAccessor;
 
+/**
+ * Messaging client interface that uses the MessagingTemplate encapsulating the to-Broker channel of a
+ * Spring STOMP relay over WebSocket component.
+ * 
+ * TODO: I think this is more aptly considered to be a Gateway than an Adapter, in Spring terms.  Consider
+ *       renaming and re-wiring for clarity and a more suitable implementation-oriented feature set.
+ *       
+ * @author jheinnic
+ *
+ */
 public class LegacyGameSrvrClient {
     private final SimpMessagingTemplate brokerMessageTemplate;
     
     private static final int HEADER_SIZE = 12;
     private static final Pattern DESTINATION_PATTERN = 
-    	Pattern.compile("^/app/gameserver/(\\d+)$");
+    	Pattern.compile("^(/app)?/gameserver/(\\d+)$");
 
     LegacyGameSrvrClient( SimpMessagingTemplate brokerMessageTemplate )
     {
@@ -28,40 +34,32 @@ public class LegacyGameSrvrClient {
     
     public void sendRequest( Message<com.google.protobuf.Message> request )
     {
-    	final com.google.protobuf.Message payload =
-    		request.getPayload();
-    	final byte[] payloadBytes = 
-    		payload.toByteArray();
-    	final int payloadSize = 
-    		payloadBytes.length;
+    	final com.google.protobuf.Message payload = request.getPayload();
+    	final byte[] payloadBytes = payload.toByteArray();
+    	final int payloadSize = payloadBytes.length;
     	
-    	try {
-			final StompHeaderAccessor headers =
-				StompHeaderAccessor.wrap(request);
-			final MobstersHeaderAccessor mobsterHeaders =
-				MobstersHeaderAccessor.wrap(headers);
-			final int tag = 
-				mobsterHeaders.getSequenceTag();
-			final Matcher m = 
-				DESTINATION_PATTERN.matcher(
-					headers.getDestination());
-			final int reqIndex =
-				Integer.parseInt(
-					m.group(1));
-			
-			final ByteArrayOutputStream baos =
-				new ByteArrayOutputStream(HEADER_SIZE + payloadSize);
-			final DataOutputStream output =
-				new DataOutputStream(baos);
+		final StompHeaderAccessor headers =
+			StompHeaderAccessor.wrap(request);
+		final MobstersHeaderAccessor mobsterHeaders =
+			MobstersHeaderAccessor.wrap(headers);
 
-			output.write(reqIndex);
-			output.write(tag);
-			output.write(payloadSize);
-			output.write(payloadBytes);
+		final int tag = mobsterHeaders.getSequenceTag();
 
-			brokerMessageTemplate.convertAndSend("/queue/clientMessages", payloadBytes);
-		} catch (IOException e) {
-			throw new StompConversionException("Failed to encode STOMP frame",  e);
-		}
+		final Matcher m = 
+			DESTINATION_PATTERN.matcher(
+				headers.getDestination());
+		final int reqIndex =
+			Integer.parseInt(
+				m.group(2));
+		
+		final byte[] legacyPayload = new byte[HEADER_SIZE + payloadSize];
+		final ByteBuffer output = ByteBuffer.wrap(legacyPayload);
+
+		output.putInt(reqIndex);
+		output.putInt(tag);
+		output.putInt(payloadSize);
+		output.put(payloadBytes);
+
+		brokerMessageTemplate.convertAndSend("/amq-queue/clientMessages", legacyPayload);
     }
 }
